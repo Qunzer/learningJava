d2770eb1ea5c1bd4fcba7ae8d4f19f2e33245132
renqun.yuan <yuanrq2011@126.com>
2015-10-15 21:09:34 +0800

    add guavaExample model

---
+++ guavaExample/src/main/java/com/javalearning/base/SuppliersExample.java
@@ -0,0 +1,32 @@
+package com.javalearning.base;
+
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by renqun.yuan on 2015/7/17.
+ */
+public class SuppliersExample {
+    private static final Supplier<String> supplier = new Supplier<String>() {
+        @Override
+        public String get() {
+            System.out.println("supplier get invoke");
+            return "supplier";
+        }
+    };
+
+    public static void main(String[] args) throws InterruptedException {
+        Supplier<String> memoizeSupplier = Suppliers.memoize(supplier);
+        System.out.println(memoizeSupplier.get());
+        System.out.println(memoizeSupplier.get());
+
+        System.out.println("=====================");
+
+        Supplier<String> memoizeWithExpiration = Suppliers.memoizeWithExpiration(supplier, 100, TimeUnit.MICROSECONDS);
+        System.out.println(memoizeWithExpiration.get());
+        System.out.println(memoizeWithExpiration.get());
+        System.out.println(memoizeWithExpiration.get());
+    }
+}
---
+++ guavaExample/src/main/java/com/javalearning/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by renqun.yuan on 2015/10/15.
+ */
+package com.javalearning;
