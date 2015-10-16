1fea31a6cef50a43500921902803606eb55aba5d
renqun.yuan <yuanrq2011@126.com>
2015-10-16 20:02:42 +0800

    add bimapExample

---
+++ guavaExample/src/main/java/com/javalearning/collection/BiMapExample.java
@@ -0,0 +1,36 @@
+package com.javalearning.collection;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.ImmutableBiMap;
+
+/**
+ * Created by renqun.yuan on 2015/10/16.
+ */
+public class BiMapExample {
+    public static void hashBiMapExample() {
+        HashBiMap<String, String> biMap = HashBiMap.create();
+        biMap.put("foo", "boy");
+//        biMap.put("boy", "boy"); //IllegalArgumentException: value already present: boy
+//        biMap.forcePut("boy", "boy");// ok, foo=boy is removed
+        System.out.println(biMap);
+
+        BiMap<String, String> inverse = biMap.inverse();
+
+        System.out.println(inverse);
+
+    }
+
+    public static void immutableBiMap() {
+        ImmutableBiMap<Object, Object> immutableBiMap = ImmutableBiMap.builder().put("foo", "bar").build();
+
+        System.out.println(immutableBiMap);
+
+        System.out.println(immutableBiMap.inverse());
+    }
+
+    public static void main(String[] args) {
+        hashBiMapExample();
+        immutableBiMap();
+    }
+}
