e9b12c32c45901cf934ea9d56f78021aa774ef9f
renqun.yuan <yuanrq2011@126.com>
2015-10-19 20:28:33 +0800

    add table example

---
+++ guavaExample/src/main/java/com/javalearning/collection/TableExample.java
@@ -0,0 +1,47 @@
+package com.javalearning.collection;
+
+import com.google.common.collect.ArrayTable;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.ImmutableTable;
+import com.google.common.collect.Lists;
+
+/**
+ * Created by renqun.yuan on 2015/10/19.
+ */
+public class TableExample {
+    public static void arrayTable() {
+        ArrayTable<String, String, Integer> arrayTable = ArrayTable.<String, String, Integer>create(Lists.newArrayList("hello"), Lists.newArrayList("test"));
+        Integer value = arrayTable.get("hello", "test");
+        System.out.println(value);
+
+//      arrayTable.put("hello", "world", 2); // IllegalArgumentException: Column must in the column list
+
+        arrayTable.put("hello", "test", 2); // ok
+        System.out.println(arrayTable.get("hello", "test"));
+
+    }
+
+    public static void hashBasedTable() {
+        HashBasedTable<String, String, String> hashBasedTable = HashBasedTable.create();
+        hashBasedTable.put("hello", "world", "java");
+        System.out.println(hashBasedTable.get("hello", "world"));
+
+        hashBasedTable.put("hello", "world", "python");
+        System.out.println(hashBasedTable.get("hello", "world"));
+    }
+
+    private static void immutableTable() {
+        ImmutableTable.Builder<String, String, String> builder = ImmutableTable.builder();
+        builder.put("hello", "world", "java");
+        builder.put("hello", "xxxx", "python");
+        ImmutableTable<String, String, String> immutableTable = builder.build();
+        System.out.println(immutableTable.size());
+        System.out.println(immutableTable.get("hello", "world"));
+    }
+
+    public static void main(String[] args) {
+        arrayTable();
+        hashBasedTable();
+        immutableTable();
+    }
+}
