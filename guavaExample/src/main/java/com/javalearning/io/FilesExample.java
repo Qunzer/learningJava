c3e8eed163e983de2421d397bcefd9b9e2ad466a
renqun.yuan <yuanrq2011@126.com>
2015-10-21 21:00:27 +0800

    add files md5

---
+++ guavaExample/src/main/java/com/javalearning/io/FilesExample.java
@@ -0,0 +1,18 @@
+package com.javalearning.io;
+
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by renqun.yuan on 2015/10/21.
+ */
+public class FilesExample {
+    public static String generateFileMd5(File file) throws IOException {
+        HashCode hash = Files.hash(file, Hashing.md5());
+        return hash.toString();
+    }
+}
